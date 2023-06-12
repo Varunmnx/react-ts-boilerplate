359bf0ca42f5be222bd2d1101c946128fa418d82
varun__narayanan <varun@elsyslabs.in>
2023-06-12 23:17:46 +0530

    react ts boiler plate

---
+++ src/components/layouts/layout.tsx
@@ -0,0 +1,13 @@
+import "./common.css"
+
+const layout = () => {
+
+  return (
+    <div className="flex flex-column gap-20">
+        
+    
+    </div>
+  )
+}
+
+export default layout
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div className="w-full h-[100vh] bg-black flex justify-center items-center">
+            <h1 className="text-white font-sans text-[24px] font-bold ">Hello world</h1>
+    </div>
+  )
+}
+
+export default Home
